11d2beb4d9633ac07ac45fee86229449bca38b8e
Md Abdullah Ibn Noor <ab.89noor@gmail.com>
2024-10-14 11:43:36 +0600

    first commit

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,116 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "/home.png",
+        label: "Home",
+        href: "/",
+      },
+      {
+        icon: "/teacher.png",
+        label: "Teachers",
+        href: "/dashboard/teachers",
+      },
+      {
+        icon: "/student.png",
+        label: "Students",
+        href: "/dashboard/students",
+      },
+      {
+        icon: "/parent.png",
+        label: "Parents",
+        href: "/dashboard/parents",
+      },
+      {
+        icon: "/class.png",
+        label: "Classes",
+        href: "/dashboard/classes",
+      },
+      {
+        icon: "/class.png",
+        label: "Classes",
+        href: "/dashboard/classes",
+      },
+      {
+        icon: "/lesson.png",
+        label: "Lessons",
+        href: "/dashboard/lessons",
+      },
+      {
+        icon: "/exam.png",
+        label: "Exams",
+        href: "/dashboard/exams",
+      },
+      {
+        icon: "/assignment.png",
+        label: "Assignments",
+        href: "/dashboard/assignments",
+      },
+      {
+        icon: "/attendance.png",
+        label: "Attendance",
+        href: "/dashboard/attendance",
+      },
+      {
+        icon: "/calendar.png",
+        label: "Events",
+        href: "/dashboard/events",
+      },
+      {
+        icon: "/message.png",
+        label: "Messages",
+        href: "/dashboard/messages",
+      },
+      {
+        icon: "/announcement.png",
+        label: "Announcements",
+        href: "/dashboard/announcements",
+      },
+    ]
+  },
+  {
+    title: "OTHER",
+    items: [
+      {
+        icon: "/profile.png",
+        label: "Profile",
+        href: "/dashboard/profile",
+      },
+      {
+        icon: "/setting.png",
+        label: "Settings",
+        href: "/dashboard/settings",
+      },
+      {
+        icon: "/logout.png",
+        label: "logout",
+        href: "/logout",
+      },
+    ],
+  },
+]
+
+const Menu = () => {
+  return (
+    <div className='mt-4 text-sm text-gray-50'>
+      {menuItems.map(i => (
+        <div className="flex flex-col justify-start space-y-2 items-start ml-4" key={i.title}>
+          <span className="font-semibold my-2">{i.title}</span>
+          <hr/>
+          {i.items.map(item => (
+            <Link href={item.href} key={item.label} className="flex gap-2 items-center justify-center">
+              <Image src={item.icon} alt="" width={17} height={17} />
+              <span>{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+} 
+
+export default Menu
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "../components/Menu";
+
+export default function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="h-screen flex">
+            {/* Left */}
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-gray-700">
+                <Link href="/" className="flex gap-2 items-center m-2">
+                    <Image
+                        src="/logo.png"
+                        alt="image"
+                        width={32}
+                        height={32}
+                        color="white"
+                    />
+                    <span className="font-bold hidden lg:block lg:text-xl 2xl:text-2xl text-gray-50">
+                        School App
+                    </span>
+                </Link>
+                <Menu/>
+            </div>
+            {/* right */}
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-100">r</div>
+        </div>
+    );
+}
