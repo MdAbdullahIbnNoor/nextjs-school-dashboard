32e7402f73a75d364b23ace35fc293ff12d2bb1d
Md. Abdullah Ibn Noor <ab.89noor@gmail.com>
2024-10-15 12:20:52 +0600

    chart adding in admin dashboard

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,17 +1,30 @@
 import UserCard from "@/app/components/UserCard"
+import CountChart from '../../components/CountChart';
 
 const AdminPage = () => {
   return (
     <div className='p-2 flex-col md:flex-row'>
       {/* Left */}
       <div className="w-full lg:w-2/3">
-      {/* User Cards */}
-      <div className="flex gap-4 justify-between flex-wrap">
-        <UserCard type="student"/>
-        <UserCard type="teacher"/>
-        <UserCard type="parent"/>
-        <UserCard type="staff"/>
-      </div>
+        {/* User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* Middle Charts */}
+        <div className="flex gap-4 flex-col lg:flex-row mt-4">
+          {/* Count Charts */}
+          <div className="w-full lg:w-1/3 h-[450px] rounded-xl bg-white">
+            <CountChart />
+          </div>
+          {/* Attendence Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom Charts */}
+        <div>
+        </div>
       </div>
       {/* Right */}
       <div className="w-full lg:w-1/3"></div>
